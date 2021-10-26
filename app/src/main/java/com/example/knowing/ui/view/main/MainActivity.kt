ca1d6c5f7d5bd449ba33c6e3f07d86a6cf4ecf8c
godgo <godgod153@gmail.com>
2021-10-26 11:09:19 +0900

    메인 화면 틀 작업중

---
+++ app/src/main/java/com/example/knowing/ui/view/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.knowing.ui.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.knowing.R
+import com.example.knowing.databinding.ActivityMainBinding
+import com.example.knowing.ui.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
